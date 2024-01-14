2bfde3b9abeadfd3025588cab34678eb5fe55336
wiltslon <wiltslon@gmail.com>
2024-01-14 20:26:08 -0300

    Fim do curso Node.js: criando uma API Rest com Express e MongoDB

---
+++ src/controllers/autorController.js
@@ -0,0 +1,59 @@
+import { autor } from "../models/Autor.js";
+
+class AutorController {
+
+    static async listarAutores(req, res) {
+        try{
+            const listaAutores = await autor.find({});
+            res.status(200).json(listaAutores);
+        }catch(erro){
+            res.status(500).json({ message: `${erro.message} - falha na requisição`})
+        }
+        
+    };
+
+    static async listarAutorPorId(req, res) {
+        try{
+            const id = req.params.id;
+            const autorEncontrado = await autor.findById(id);
+            res.status(200).json(autorEncontrado);
+        }catch(erro){
+            res.status(500).json({ message: `${erro.message} - falha na requisição do autor`})
+        }
+        
+    };
+
+    static async cadastrarAutor(req, res) {
+        try {
+            const novoAutor = await autor.create(req.body);
+            res.status(201).json({ message: "Criado com sucesso", autor: novoAutor });
+
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha ao cadastrar o autor`});
+        }
+    };
+
+    static async atualizarAutor(req, res) {
+        try{
+            const id = req.params.id;
+            await autor.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: "Autor atualizado com sucesso!"});
+        }catch(erro){
+            res.status(500).json({ message: `${erro.message} - falha na atualização`});
+        }
+        
+    };
+
+    static async deletarAutor(req, res) {
+        try{
+            const id = req.params.id;
+            await autor.findByIdAndDelete(id);
+            res.status(200).json({ message: "Autor deletado com sucesso!"});
+        }catch(erro){
+            res.status(500).json({ message: `${erro.message} - falha na exclusão`});
+        }
+        
+    };
+};
+
+export default AutorController;
